3cfae9a7fd72f7854240388c81e7cc9f4d8130d3
Stephen Moore <stermmoore@gmail.com>
2022-09-18 15:44:10 +0100

    added jest mock

--- jest/request-handler.js
+++ jest/request-handler.js
@@ -1,5 +1,15 @@
 module.exports = class RequestHandler {
+
+    constructor(myRepository) {
+        this.repository = myRepository;
+    }
+
     handle = function(name) {
         return `hello ${name}`
+    }
+
+    getData = function() {
+        console.log(this.repository)
+        return this.repository.getData();
     }
 }
--- jest/request-handler.test.js
+++ jest/request-handler.test.js
@@ -2,11 +2,24 @@ const { hasUncaughtExceptionCaptureCallback } = require("process");
 const RequestHandler = require("./request-handler");
 
 describe("requestHandler", () => {
+
+    const mockRepository = { getData : jest.fn() }
+
+
     test("first test", () => {
-        const requestHandler = new RequestHandler();
+        const requestHandler = new RequestHandler(mockRepository);
 
         var result = requestHandler.handle("dave");
 
         expect(result).toBe("hello dave");
+    })
+
+    test("Mock Repository Is Called", () => {
+        const requestHandler = new RequestHandler(mockRepository);
+
+        const result = requestHandler.getData();
+
+        expect(mockRepository.getData).toHaveBeenCalled();
+
     })
 })
